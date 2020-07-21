ac6c1d4c53aba6641d5d0cdd7bd01805bd66918f
wolf-math <aaron.wolf@gmail.com>
2020-07-21 18:54:06 +0300

    hero image

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './homepage.style.scss';
+import logo from '../../assets/hero-logo.png'
 
 const homepage = () => (
     <div className="home">
         <div className="hero">
             <div className="hero-text">
+                <img id='main-logo' src={logo} alt="logo" />
                 <h1>Caya Coffee Co.</h1>
                 <h3>Artisan Coffee</h3>
             </div>
