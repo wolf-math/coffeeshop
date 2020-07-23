567dac0df54da8f3fbffb59e6f9aef4a36df8577
wolf-math <aaron.wolf@gmail.com>
2020-07-23 23:32:03 +0300

    homepage divs

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import './homepage.style.scss';
 import logo from '../../assets/hero-logo.png';
+import cup from '../../assets/cup.jpg';
+import mugs from '../../assets/mugs.jpg';
 
 const homepage = () => (
     <div className="home">
@@ -12,8 +14,18 @@ const homepage = () => (
                 <h3>Specialty Coffee and Tea</h3>
             </div>
         </div>
-        <div className="content">
-            
+        <div className="blurb1">
+            <img className="cup" src={cup} alt="cup" />
+            <div className="blurb1-text">
+                <h3>A little about us</h3>
+                <p>We like tasty snacks, not vegtables!</p>
+            </div>
+        </div>
+        <div className="blurb2">
+            <div className="blurb2-text">
+                <h1>hey</h1>
+            </div>
+            <img className="mugs" src={mugs} alt="mugs" />
         </div>
     </div>
 )
