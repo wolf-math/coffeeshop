d5c635b827cd1ab069976ac4bf655edb8496572d
wolf-math <aaron.wolf@gmail.com>
2020-07-27 11:00:17 +0300

    homepage style + content

--- src/pages/homepage/homepage.page.jsx
+++ src/pages/homepage/homepage.page.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './homepage.style.scss';
 import logo from '../../assets/hero-logo.png';
@@ -15,16 +16,20 @@ const homepage = () => (
             </div>
         </div>
         <div className="content">
-            <div className="blurb1">
+            <div className="blurb blurb1">
                 <img className="blurb-pic" src={cup} alt="cup" />
                 <div className="blurb1-text">
                     <h3>A little about us</h3>
                     <p>We like tasty snacks, not vegtables!</p>
+                    <Link className='option' to='about'>Learn More</Link>
                 </div>
             </div>
-            <div className="blurb2">
+            <div className="blurb blurb2">
                 <div className="blurb2-text">
-                    <h1>hey</h1>
+                    <h1>Our coffee is better than yours</h1>
+                    <p>Taste it to belive it</p>
+                    <Link className='option' to='about'>Learn More</Link>
+
                 </div>
                 <img className="blurb-pic" src={mugs} alt="mugs" />
             </div>
