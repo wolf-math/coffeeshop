30af382f031328a050cc69fd7b56f9884d7ae8e3
aaron <aaron.wolf@gmail.com>
2021-02-11 09:43:46 +0200

    hamburger navbar

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,33 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
-// import {Navbar as Nav } from 'react-bootstrap'
+import { FaBars } from 'react-icons/fa'
+import Fade from 'react-reveal/Fade';
+
 
 import './navbar.style.scss';
-import logo from '../../assets/horizontal_logo_brown.png';
 
-const Navbar = () => (
-    <div className="navbar">
-        <div className="nav-logo">
-            <Link to='/'>
-                <img id="nav-logo" src={logo} alt=""/>
-            </Link>
-        </div>
+const Navbar = () => {
 
-        <div className='options'>
-            <Link className='option' to='/'>
-                home
-            </Link>
-            <Link className='option' to='about'>
-                about
-            </Link>
-            <Link className='option' to='drinks'>
-                menu
-            </Link>
-            <Link className='option' to='contact'>
-                contact
-            </Link>
+    const [visible, setVisible] = useState(false);
+    const [navStyle, setNavStyle] = useState('nav')
+
+    const toggle = () => {
+        setVisible(!visible)
+        setNavStyle(visible ? 'nav' : 'nav-click')
+    }
+
+    
+
+    const menu = (
+        <div onClick={toggle}>
+            <Fade><a className='option' href='/'>home</a></Fade>
+            <Fade><a className='option' href='about'>about</a></Fade>
+            <Fade><a className='option' href='drinks'>drinks</a></Fade>
+            <Fade><a className='option' href='contact'>contact</a></Fade>
+        </div>); 
+
+    return(
+        <div className={navStyle}>
+            <FaBars className="menu" onClick={toggle} />
+            {!visible ? null : menu}
         </div>
-    </div>
-)
+    )
+}
 
 export default Navbar;
